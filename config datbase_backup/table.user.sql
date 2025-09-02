8a197698387d538945772cfeb503fc44c90d22cc
Camilo Esteban Puchana <camilopuchana-10@hotmail.com>
2025-09-02 15:36:36 -0500

    creacion tabla

---
+++ config datbase_backup/table.user.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users(
+            id BIGSERIAL PRIMARY KEY,
+            firstname VARCHAR(30) NOT NULL,
+            lastname VARCHAR(30) NOT NULL,
+            mobile_number VARCHAR(20) NOT NULL,
+            ide_number VARCHAR(15) NULL UNIQUE,
+            address TEXT NULL,
+            birthday DATE NULL,
+            email VARCHAR(200) NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            status BOOLEAN NOT NULL DEFAULT TRUE,
+            created at TIMESTAMPtZ NOT NULL DEFAULT now(),
+            updated at TIMESTAMPtZ NOT NULL DEFAULT now(),
+            deleted at TIMESTAMPtZ NULL
+
+
+);
+
+insert into users(firstname, lastname, mobile_number, email, password) 
+values('Joan C','Ayala','465133','joan@mail.com','1234');
